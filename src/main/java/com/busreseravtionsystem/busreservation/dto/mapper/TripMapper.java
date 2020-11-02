feb09e2833a0bdd8234b5b38f1b9f157921346b5
Bankass77 <amguindo77@gmail.com>
2020-11-02 15:48:21 +0100

    TriMapper, BusReservationService, BusreservationApplication

--- src/main/java/com/busreseravtionsystem/busreservation/dto/mapper/TripMapper.java
+++ src/main/java/com/busreseravtionsystem/busreservation/dto/mapper/TripMapper.java
@@ -4,22 +4,14 @@ import com.busreseravtionsystem.busreservation.dto.bus.TripDto;
 import com.busreseravtionsystem.busreservation.model.bus.Trip;
 
 public class TripMapper {
-	
+
 	public static TripDto toTripMapper(Trip trip) {
-		
-		
-		
-		return new  TripDto()
-				.setId(trip.getId())
-				.setAgencyCode(trip.getBus().getAgency().getCode())
-				.setBusCode(trip.getBus().getCode())
-				.setDestinationStopCode(trip.getDestStop().getCode())
-				.setDestinationStopName(trip.getSourceStop().getName())
-				.setFare(trip.getFare())
-				.setJourneyTime(trip.getJourneyTime())
-				.setSourceStopCode(trip.getSourceStop().getCode());
-		
-		
+
+		return new TripDto().setId(trip.getId()).setAgencyCode(trip.getBus().getAgency().getCode())
+				.setBusCode(trip.getBus().getCode()).setDestinationStopCode(trip.getDestStop().getCode())
+				.setDestinationStopName(trip.getSourceStop().getName()).setFare(trip.getFare())
+				.setJourneyTime(trip.getJourneyTime()).setSourceStopCode(trip.getSourceStop().getCode());
+
 	}
 
 }
